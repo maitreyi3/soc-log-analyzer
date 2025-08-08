54d0edfc7ea6474041ecc1f7226daadafe307c59
Maitreyi Ekbote <maitreyi2001ekbote@gmail.com>
2025-08-07 19:55:27 -0700

    Add Docker setup and clean up

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Skip lint errors during production builds
+  },
 };
 
 export default nextConfig;
